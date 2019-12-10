998c61c7303436233877ea7603caaf1ae7e447c8
Yiet Mai <ymai@ggc.edu>
2019-12-10 15:46:19 -0500

    create project with sample ui label

---
+++ ProjectPrep/ViewController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProjectPrep
+//
+//  Created by Yiet Mai on 12/10/19.
+//  Copyright © 2019 Yiet Mai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = "GitHub Users "
+        print("hello Mark")
+        
+    }
+
+
+}
+
