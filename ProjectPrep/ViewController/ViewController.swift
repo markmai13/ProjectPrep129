139ca8dcec6b268a9077858e82bdc03d0bca64e9
Yiet Mai <ymai@ggc.edu>
2019-12-11 09:24:49 -0500

    adding second view controller and table view

--- ProjectPrep/ViewController/ViewController.swift
+++ ProjectPrep/ViewController/ViewController.swift
@@ -9,15 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    @IBOutlet weak var label: UILabel!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label.text = "GitHub Users "
         print("hello Mark")
         
     }
